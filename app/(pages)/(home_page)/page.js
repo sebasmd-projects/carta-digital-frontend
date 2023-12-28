80222c79b90b917ada9f1120995ca147769ccc05
SebasMD <sebasmoralesd@gmail.com>
2023-12-27 21:15:48 -0500

    Proyecto base next.js - jsconfig - package.json - sass - estructura de carpetas (aplicaciones modulares tipo django) - Sin contenido extra

---
+++ app/(pages)/(home_page)/page.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Carta digital',
+  description: 'Pagina inicial',
+}
+
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen">
+      <h1>HomePage</h1>
+    </main>
+  )
+}
